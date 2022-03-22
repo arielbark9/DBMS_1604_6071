8373b40262cb7a1501374f741da86eca67d3ebfb
arielbark9 <arielbark9@gmail.com>
2022-03-22 17:09:18 +0200

    added ;

--- Hw2/equipment_gen.py
+++ Hw2/equipment_gen.py
@@ -6,7 +6,7 @@ with open("Equipment_inserts.sql","w") as sqlfile:
     result = ""
     for i in range(1, 20001):
         equipmentName = faker.bothify(text='????-########')
-        result += f"insert into Equipment values({i}, '{equipmentName}')\n"
+        result += f"insert into Equipment values({i}, '{equipmentName}');\n"
     sqlfile.write(result)
 
 with open("Renewing_Equipment_inserts.sql","w") as sqlfile:
@@ -15,7 +15,7 @@ with open("Renewing_Equipment_inserts.sql","w") as sqlfile:
         lastRenew = faker.date_object().strftime("%d/%m/%Y")
         branchID = faker.random_int(1, 1001)
         equipmentID = faker.random_int(1, 20001)
-        result += f"insert into Renewing_Equipment values({i}, TO_DATE('{lastRenew}','dd/mm/yyyy'), {branchID}, {equipmentID})\n"
+        result += f"insert into Renewing_Equipment values({i}, TO_DATE('{lastRenew}','dd/mm/yyyy'), {branchID}, {equipmentID});\n"
     sqlfile.write(result)
 
 with open("Permanent_Equipment_inserts.sql","w") as sqlfile:
@@ -24,5 +24,5 @@ with open("Permanent_Equipment_inserts.sql","w") as sqlfile:
         lastMaintenance = faker.date_object().strftime("%d/%m/%Y")
         branchID = faker.random_int(1, 1001)
         equipmentID = faker.random_int(1, 20001)
-        result += f"insert into Permanent_Equipment values({i}, TO_DATE('{lastMaintenance}','dd/mm/yyyy'), {branchID}, {equipmentID})\n"
+        result += f"insert into Permanent_Equipment values({i}, TO_DATE('{lastMaintenance}','dd/mm/yyyy'), {branchID}, {equipmentID});\n"
     sqlfile.write(result)
